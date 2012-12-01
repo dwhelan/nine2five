2112f0acda62ae2491d60135adf2ee06b22d8128
Declan Whelan <declan@printchomp.com>
2012-12-01 17:34:02 -0500

    Refactored dsl examples

--- spec/nine2five/dsl/examples_spec.rb
+++ spec/nine2five/dsl/examples_spec.rb
@@ -4,29 +4,15 @@ module Nine2Five
 
   describe "DSL Examples" do
 
-    before { initialize_workflow_map }
-
     it "should enable a simple adder" do
       adder = eval("
-        a = c :a, 1
-        b = c :b, 2
-        sum = c :c
-        p(:p, [a, b], sum) {|x| x.inject(:+)}"
+        a = c in: 1
+        b = c in: 2
+        p(in: [a, b]) {|x| x.inject(:+)}"
       )
 
       adder.process.should be 3
     end
-
-    it "should enable a simple adder" do
-      pending 'blah'
-      adder = eval("
-        p(:p, [1, 2]) {|x| x.inject(:+)}"
-      )
-
-      adder.process.should be 3
-    end
-
-
 
   end
 end
